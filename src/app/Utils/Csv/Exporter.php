e213016a9b28035fe8b1ec7fdf3f47e6f9cdbfa3
dragon99k <inddev0807@gmail.com>
2023-09-16 17:27:35 +0900

    feat: utilities

---
+++ src/app/Utils/Csv/Exporter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Utils\Csv;
+
+interface Exporter
+{
+    /**
+     * @var array
+     */
+    public const DOWNLOAD_CSV_HEADER = [
+        'Content-Type' => 'text/csv',
+    ];
+
+    public const DOWNLOAD_XML_HEADER = [
+        'Content-Type' => 'application/xml',
+    ];
+
+    /**
+     * @param array $headers
+     *
+     * @return static
+     */
+    public function setHeaders(array $headers);
+
+    /**
+     *
+     * @return array
+     */
+    public function getHeaders();
+
+    /**
+     * @param string $encodeing
+     *
+     * @return static
+     */
+    public function setOutputEncoding(string $encodeing);
+
+    /**
+     * @param \Closure $provider
+     * @param string|resource $outputDest
+     *
+     * @return \App\Utils\FileExporter
+     */
+    public function getExporter(\Closure $provider, $outputDest = 'php://output'): \App\Utils\FileExporter;
+
+    /**
+     * @param \Closure $provider
+     * @param string|resource $outputDest
+     *
+     * @return resource
+     */
+    public function createStream(\Closure $provider, $outputDest = 'php://output');
+}
