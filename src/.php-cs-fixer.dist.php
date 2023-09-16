3484c7e75cb13577fdc0fe28507f2903fff46df9
dragon99k <inddev0807@gmail.com>
2023-09-16 17:55:29 +0900

    feat: php-cs-fixer

---
+++ src/.php-cs-fixer.dist.php
@@ -0,0 +1,28 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR2' => true,
+        '@Symfony' => true,
+        'no_superfluous_phpdoc_tags' => false,
+        'yoda_style' => false,
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_summary' => false,
+        'phpdoc_no_package' => false,
+        'array_indentation' => true,
+        'single_line_throw' => false,
+        'concat_space' => false,
+        'increment_style' => false,
+        'new_with_braces' => false,
+        'single_trait_insert_per_statement' => false,
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->exclude('bootstrap/cache')
+        ->exclude('resources')
+        ->exclude('storage')
+        ->exclude('node_modules')
+        ->exclude('tools')
+        ->in(__DIR__)
+    );
